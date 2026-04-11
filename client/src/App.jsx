03414383bfa1da34e8b9895b1bbd665f878fa822
Anovuyo Jafta <anovuyojafta@gmail.com>
2026-04-11 23:55:07 +0200

    registration page UI

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Registration from './Registration'
+import './App.css'
+
+function App() {
+  
+  return (
+    <Registration />
+   )
+}
+
+export default App
