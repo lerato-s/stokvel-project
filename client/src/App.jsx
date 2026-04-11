42f6af13636425e526d7c0dc1952bff35d7cebaf
Anovuyo Jafta <anovuyojafta@gmail.com>
2026-04-11 23:59:05 +0200

    registration page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,20 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Registration from './Registration'
 import './App.css'
+import background from './background.jpg'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 
 function App() {
   
   return (
-    <Registration />
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+    
    )
 }
 
