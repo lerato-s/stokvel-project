1fccccae49400aea53dfd18900c361aaddb17545
lerato-s <sikhumbanal@gmail.com>
2026-04-18 22:57:18 +0200

    add loading indicator to login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,6 +9,7 @@ function Login() {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); // checks whether login request is happening or not -> starts at false 
   const navigate = useNavigate();
 
   // Add this at the top of handleSubmit to confirm state is populating
@@ -16,6 +17,8 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true); // when user clicks login, isLoading is now true
+    setErrorMessage("");
 
     try {
       const result = await axios.post(`${import.meta.env.VITE_API_URL}/login`, {
@@ -41,6 +44,8 @@ function Login() {
   }catch (error) {
       console.error("Error logging in:", error);
       setErrorMessage("Invalid email or password");
+    } finally {
+      setIsLoading(false); // failed or nah
     }
   };
 
@@ -52,6 +57,7 @@ function Login() {
         setPassword={setPassword}
         showPassword={showPassword}
         setShowPassword={setShowPassword}
+        isLoading={isLoading}  // passed to LoginForm so we can change btn
     />
   );
 }
