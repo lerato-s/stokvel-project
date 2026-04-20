4dd68c43065207eaf042c6752df8fa65c5a51c05
Nessaforealz <busiwe2006@gmail.com>
2026-04-20 10:05:48 +0200

    changed css file

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../pages/login.css";
+import "../pages/Login.css";
 import LoginForm from "../components/LoginForm";
 
 function Login() {
